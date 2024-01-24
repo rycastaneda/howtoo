2284a0c2c5c9503223f2314594100d60a7e9d504
Ryan Castañeda <castaneda.ry@gmail.com>
2024-01-24 16:12:09 +0800

    Initial commit for HowToo test questions

---
+++ part2/practice.js
@@ -0,0 +1,109 @@
+function dummy () {
+  return 1;
+}
+/**
+ * Reverse an array
+ * 
+ * @example
+ * 
+ * reverse([1,2,3]) returns [3,2,1]
+ * 
+ * @param {array} array
+ * @returns {array}
+ */
+function reverse (array) {
+  return [];
+}
+
+/**
+ * Reverse a string
+ * 
+ * @example
+ * 
+ * stringReverse('hello') returns 'olleh'
+ * 
+ * @param {string} text
+ * @returns {string}
+ * 
+ */
+function stringReverse (text) {
+  return '';
+}
+
+
+/**
+ * return a fixed length fibonacci list
+ * 
+ * @example
+ * 
+ * fibonacci(5) returns [1,1,2,3,5]
+ * 
+ * @param {number} length
+ * @returns {array}
+ */
+function fibonacci (length) {
+  return [];
+}
+
+/**
+ * Given an array of integers nums and an integer target,
+ * return arraries of the two numbers such that they add up to target.
+ * 
+ * @example
+ * 
+ * twoSum([1,3,5,7,9], 8) returns [[1, 7], [3, 5]]
+ * 
+ * @param {number[]} testArray
+ * @param {number} target
+ * @returns {number[]}
+ */
+function twoSum (testArray, target) {
+  return [];
+}
+
+/**
+ * Creates an array of numbers progressing from 
+ * start up to, but not including, end
+ * 
+ * YOU HAVE TO IMPLEMENT YOUR OWN ALGORITHM HERE!!!
+ * DON'T USE ANY 3RD PARTY LIBRARIES.
+ * 
+ * @example
+ * 
+ * range(0,4) returns [0,1,2,3]
+ * 
+ * @param {number} start 
+ * @param {number} end 
+ * @returns {array}
+ */
+function range (start, end) {
+  return [];
+}
+
+/**
+ * Recursively flattens array.
+ * 
+ * YOU HAVE TO IMPLEMENT YOUR OWN ALGORITHM HERE!!!
+ * DON'T USE ANY 3RD PARTY LIBRARIES.
+ * 
+ * @example
+ * 
+ * flatten([1,[2,[3,4],[5,[6]]]]) returns [1,2,3,4,5,6]
+ * 
+ * @param {array} array
+ * @returns {array}
+ */
+function flatten (array) {
+  return [];
+}
+
+module.exports = {
+  dummy,
+  fibonacci,
+  reverse,
+  flatten,
+  twoSum,
+  flatten,
+  stringReverse,
+  range
+};
